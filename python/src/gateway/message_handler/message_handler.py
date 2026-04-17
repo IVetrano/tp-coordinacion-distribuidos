e34f69a8da1427b27d1e3c24644d4ffb1b0e46ad
Ignacio Vetrano <ivetrano@fi.uba.ar>
2026-04-17 20:37:05 -0300

    feat: include type and query_id in serialized messages

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -1,17 +1,19 @@
 from common import message_protocol
+import uuid
 
 
 class MessageHandler:
 
     def __init__(self):
+        self.query_id = str(uuid.uuid4())
         pass
     
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        return message_protocol.internal.serialize(["DATA", self.query_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+        return message_protocol.internal.serialize(["EOF", self.query_id])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
